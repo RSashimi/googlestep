f506d2a0079e24ec8a997c3e3b5e37d642a436e8
Rena Shimada <131948585+RSashimi@users.noreply.github.com>
2025-06-09 10:33:36 +0900

    Update hw-2mentor.py

--- week-3/hw-2mentor.py
+++ week-3/hw-2mentor.py
@@ -76,6 +76,25 @@ def evaluate_multiply_divide(tokens):
     return new_tokens
 
 def evaluate_plus_minus(tokens):
+    new_tokens = []
+    new_tokens.insert(0, {'type': 'PLUS'})  
+    answer = 0
+    index = 1
+    while index < len(new_tokens):
+        if new_tokens[index]['type'] == 'NUMBER':
+            if new_tokens[index - 1]['type'] == 'PLUS':
+                answer += new_tokens[index]['number']
+            elif new_tokens[index - 1]['type'] == 'MINUS':
+                answer -= new_tokens[index]['number']
+            else:
+                print("Invalid syntax !!!!")
+                exit(1)
+        index += 1
+    return answer
+tokens_after_multiply_divide = evaluate_multiply_divide(new_tokens)
+final_answer = evaluate_plus_minus(tokens_after_multiply_divide)
+return final_answer
+
 
 
 
